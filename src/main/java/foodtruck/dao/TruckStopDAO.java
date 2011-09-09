91e14a46c15d20589b9cd68026b0309ad227bfc7
Andrew Violette <andrew@andrewviolette.net>
2011-09-08 21:02:36 -0500

    Added documentation for TruckStopDAO

--- src/main/java/foodtruck/dao/TruckStopDAO.java
+++ src/main/java/foodtruck/dao/TruckStopDAO.java
@@ -12,9 +12,18 @@ import foodtruck.model.TruckStop;
  * @since Jul 12, 2011
  */
 public interface TruckStopDAO {
+  /**
+   * Finds a set of truck stops at a particular time
+   */
   Set<TruckStop> findAt(DateTime instant);
 
+  /**
+   * Deletes all the truck stops after the specified instant
+   */
   void deleteAfter(DateTime startTime);
 
+  /**
+   * Adds the truckstops to the data store.
+   */
   void addStops(List<TruckStop> stops);
 }
