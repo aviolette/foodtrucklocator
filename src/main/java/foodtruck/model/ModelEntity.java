0ed2fd330b2201c96bd4a8225c74e84b9cb585ce
Andrew Violette <andrew@andrewviolette.net>
2011-10-16 09:40:47 -0500

    Added model entity

---
+++ src/main/java/foodtruck/model/ModelEntity.java
@@ -0,0 +1,18 @@
+package foodtruck.model;
+
+/**
+ * Provides an object's key to be used in a DB.
+ * @author aviolette@gmail.com
+ * @since 10/16/11
+ */
+public abstract class ModelEntity {
+  protected final Object key;
+
+  public ModelEntity(Object key) {
+    this.key = key;
+  }
+
+  public Object getKey() {
+    return key;
+  }
+}
