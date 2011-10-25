9814cd70d17485d215361a863082f8d7006779f1
Andrew Violette <andrew@andrewviolette.net>
2011-10-24 20:28:39 -0500

    Changed TerminationDetector to handle 'last call'.  Also, it uses the tweet time and not the clock time.

--- src/main/java/foodtruck/schedule/TerminationDetector.java
+++ src/main/java/foodtruck/schedule/TerminationDetector.java
@@ -1,11 +1,8 @@
 package foodtruck.schedule;
 
-import com.google.inject.Inject;
-
 import org.joda.time.DateTime;
 
 import foodtruck.model.TweetSummary;
-import foodtruck.util.Clock;
 
 /**
  * Detects termination messages in tweets
@@ -13,21 +10,17 @@ import foodtruck.util.Clock;
  * @since 10/20/11
  */
 public class TerminationDetector {
-  private final Clock clock;
-
-  @Inject
-  public TerminationDetector(Clock clock) {
-    this.clock = clock;
-  }
-
   // TODO: probably need an abstraction like TruckStopMatch to handle terminations
   public DateTime detect(TweetSummary tweet) {
     String tweetText = tweet.getText().toLowerCase();
+    if (tweetText.contains("last call")) {
+      return tweet.getTime().plusMinutes(15);
+    }
     if (tweetText.contains("sold out") || tweetText.contains("good-bye") ||
         tweetText.contains("good night") || tweetText.contains("good bye")
         || tweetText.contains("leaving") || tweetText.contains("heading out")
         || tweetText.contains("thanks") || tweetText.contains("thank you")) {
-      return clock.now();
+      return tweet.getTime();
     }
     return null;
   }
--- src/test/java/foodtruck/schedule/TerminationDetectorTest.java
+++ src/test/java/foodtruck/schedule/TerminationDetectorTest.java
@@ -1,59 +1,55 @@
 package foodtruck.schedule;
 
-import org.easymock.EasyMockSupport;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.junit.Before;
 import org.junit.Test;
 
 import foodtruck.model.TweetSummary;
-import foodtruck.util.Clock;
-import static org.easymock.EasyMock.expect;
 import static org.junit.Assert.assertEquals;
 
 /**
  * @author aviolette@gmail.com
  * @since 10/20/11
  */
-public class TerminationDetectorTest extends EasyMockSupport {
-  private DateTime now;
-  private Clock clock;
+public class TerminationDetectorTest {
+  private DateTime tweetTime;
   private TerminationDetector detector;
   private TweetSummary.Builder tweetBuilder;
 
   @Before
   public void before() {
-    now = new DateTime(2011, 10, 10, 9, 8, 7, 0, DateTimeZone.UTC);
-    clock = createMock(Clock.class);
-    expect(clock.now()).andStubReturn(now);
-    detector = new TerminationDetector(clock);
-    tweetBuilder = new TweetSummary.Builder();
+    tweetTime = new DateTime(2011, 10, 10, 9, 8, 7, 0, DateTimeZone.UTC);
+    detector = new TerminationDetector();
+    tweetBuilder = new TweetSummary.Builder().time(tweetTime);
   }
 
   @Test
   public void testThankYou() {
-    replayAll();
-    assertEquals(now, detector.detect(tweetBuilder.text(
+    assertEquals(tweetTime, detector.detect(tweetBuilder.text(
         "Thank you U of Chicago for braving the weather today and South Loop for closing out our day!  Enjoy the rest of your night!")
         .build()));
-    assertEquals(now, detector.detect(tweetBuilder.text(
+    assertEquals(tweetTime, detector.detect(tweetBuilder.text(
         "fidotogo: Thank you so much everyone in Andersonville! Goodnight Stockton, Prince, Toby, Shaggy, Anthony  and all the rest of our wonderful pups!...")
         .build()));
-    verifyAll();
   }
 
   @Test
   public void test2() {
-    replayAll();
-    assertEquals(now,
+    assertEquals(tweetTime,
         detector.detect(tweetBuilder.text("Thanks so very much AON&Streeterville!!").build()));
-    verifyAll();
   }
 
   @Test
   public void testGoodNight() {
-    replayAll();
-    assertEquals(now, detector.detect(tweetBuilder.text("Good night Chicago!").build()));
-    verifyAll();
+    assertEquals(tweetTime, detector.detect(tweetBuilder.text("Good night Chicago!").build()));
+  }
+
+  @Test
+  public void testLastCall_PutItAtFifteenMinutesAhead() {
+    assertEquals(tweetTime.plusMinutes(15),
+        detector.detect(
+            tweetBuilder.text("Culture: Last Call River North!!  If you are on your way, tweet us")
+                .build()));
   }
 }
