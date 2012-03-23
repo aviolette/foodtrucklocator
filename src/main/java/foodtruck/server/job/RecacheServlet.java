bd228787aa310932ce39788c0327ae75481fa32f
Andrew Violette <andrew@andrewviolette.net>
2012-03-22 21:35:02 -0500

    Allow specification of a date on the recache servlet

--- src/main/java/foodtruck/server/job/RecacheServlet.java
+++ src/main/java/foodtruck/server/job/RecacheServlet.java
@@ -1,6 +1,8 @@
 package foodtruck.server.job;
 
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -8,8 +10,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.common.base.Strings;
+import com.google.common.base.Throwables;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+
+import org.joda.time.DateTimeZone;
+import org.joda.time.LocalDate;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 import foodtruck.dao.TruckDAO;
 import foodtruck.truckstops.FoodTruckStopService;
@@ -26,25 +34,43 @@ public class RecacheServlet extends HttpServlet {
   private final Clock clock;
   private final TwitterService twitterService;
   private final TruckDAO truckDAO;
+  private final DateTimeFormatter timeFormatter;
+  private static final Logger log = Logger.getLogger(RecacheServlet.class.getName());
 
   @Inject
   public RecacheServlet(FoodTruckStopService service, Clock clock,
-      TwitterService twitterService, TruckDAO truckDAO) {
+      TwitterService twitterService, TruckDAO truckDAO, DateTimeZone zone) {
     this.twitterService = twitterService;
     this.service = service;
     this.clock = clock;
     this.truckDAO = truckDAO;
+    this.timeFormatter = DateTimeFormat.forPattern("YYYYMMdd").withZone(zone);
+
   }
 
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
     final String truck = req.getParameter("truck");
+    final String date = req.getParameter("date");
+    LocalDate when = parseDate(date);
     if (!Strings.isNullOrEmpty(truck)) {
-      service.updateStopsForTruck(clock.currentDay(), truckDAO.findById(truck));
+      service.updateStopsForTruck(when, truckDAO.findById(truck));
     } else {
-      service.updateStopsFor(clock.currentDay());
+      service.updateStopsFor(when);
     }
     twitterService.twittalyze();
+  }
+
+  private LocalDate parseDate(String date) {
+    if (!Strings.isNullOrEmpty(date)) {
+      try {
+        return timeFormatter.parseDateTime(date).toDateMidnight().toLocalDate();
+      } catch (Exception e) {
+        log.log(Level.WARNING, "Error formatting time", e);
+        Throwables.propagate(e);
+      }
+    }
+    return clock.currentDay();
   }
 }
