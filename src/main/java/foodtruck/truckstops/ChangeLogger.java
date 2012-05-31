6d6daf54f3bb3ce97a7552bad4c5ded5f6a74585
Andrew Violette <andrew@andrewviolette.net>
2012-05-31 16:25:55 -0500

    Added logging to change logger

--- src/main/java/foodtruck/truckstops/ChangeLogger.java
+++ src/main/java/foodtruck/truckstops/ChangeLogger.java
@@ -1,6 +1,9 @@
 // Copyright 2012 BrightTag, Inc. All rights reserved.
 package foodtruck.truckstops;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import com.google.inject.Inject;
 
 import foodtruck.dao.TruckStopChangeDAO;
@@ -15,6 +18,7 @@ import foodtruck.util.Clock;
 public class ChangeLogger implements TruckStopNotifier {
   private final TruckStopChangeDAO truckStopChangeDAO;
   private final Clock clock;
+  private static final Logger log = Logger.getLogger(ChangeLogger.class.getName());
 
   @Inject
   public ChangeLogger(TruckStopChangeDAO dao, Clock clock) {
@@ -23,12 +27,14 @@ public class ChangeLogger implements TruckStopNotifier {
   }
 
   @Override public void added(TruckStop stop) {
+    log.log(Level.INFO, "Stop added: {0}", stop);
     TruckStopChange.Builder changeBuilder = TruckStopChange.builder();
     changeBuilder.from(null).to(stop).timeStamp(clock.now());
     truckStopChangeDAO.save(changeBuilder.build());
   }
 
   @Override public void removed(TruckStop stop) {
+    log.log(Level.INFO, "Stop removed: {0}", stop);
     TruckStopChange.Builder changeBuilder = TruckStopChange.builder();
     changeBuilder.to(null).from(stop).timeStamp(clock.now());
     truckStopChangeDAO.save(changeBuilder.build());
