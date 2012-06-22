a750634beec050f68c6c64ce46cba64ee297dae9
Andrew Violette <andrew@andrewviolette.net>
2012-06-22 08:31:35 -0500

    Changes to CSV writer including removing of INACTIVE and DESCRIPTION, formatting of foursquare and facebook URLs, and adding EMAIL and PHONE.

--- src/main/java/foodtruck/server/resources/csv/TruckCSVCollectionWriter.java
+++ src/main/java/foodtruck/server/resources/csv/TruckCSVCollectionWriter.java
@@ -4,11 +4,11 @@ package foodtruck.server.resources.csv;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.StringWriter;
-import java.io.Writer;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
 import java.util.List;
 
+import javax.annotation.Nullable;
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
@@ -16,6 +16,7 @@ import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.MessageBodyWriter;
 import javax.ws.rs.ext.Provider;
 
+import com.google.common.base.Strings;
 import com.google.common.collect.Lists;
 
 import au.com.bytecode.opencsv.CSVWriter;
@@ -31,8 +32,8 @@ import foodtruck.server.resources.json.JSONSerializer;
 public class TruckCSVCollectionWriter implements MessageBodyWriter<Iterable<Truck>> {
 
   private static final String[] HEADER =
-      new String[] {"NAME", "TWITTER HANDLE", "FACEBOOK URI", "FOURSQUARE ID", "CATEGORIES",
-          "DESCRIPTION", "WEBSITE URL", "ACTIVE"};
+      new String[] {"NAME", "TWITTER HANDLE", "FACEBOOK URI", "FOURSQUARE ID", "WEBSITE URL",
+          "PHONE", "EMAIL"};
 
   @Override public boolean isWriteable(Class<?> type, Type genericType, Annotation[] annotations,
       MediaType mediaType) {
@@ -62,12 +63,27 @@ public class TruckCSVCollectionWriter implements MessageBodyWriter<Iterable<Truc
     List<String> entries = Lists.newArrayListWithCapacity(8);
     entries.add(truck.getName());
     entries.add(truck.getTwitterHandle());
-    entries.add(truck.getFacebook());
-    entries.add(truck.getFoursquareUrl());
-    entries.add(truck.getCategoryList());
-    entries.add(truck.getDescription());
+    entries.add(completeFacebook(truck.getFacebook()));
+    entries.add(completeFoursquare(truck.getFoursquareUrl()));
     entries.add(truck.getUrl());
-    entries.add(String.valueOf(!truck.isInactive()));
+    entries.add(truck.getPhone());
+    entries.add(truck.getEmail());
     return entries.toArray(new String[entries.size()]);
+  }
+
+  private String completeFoursquare(String foursquareUrl) {
+    foursquareUrl = Strings.emptyToNull(foursquareUrl);
+    if (foursquareUrl == null) {
+      return null;
+    }
+    return "http://www.foursquare.com/v/" + foursquareUrl;
+  }
+
+  private @Nullable String completeFacebook(String facebook) {
+    String uri = Strings.emptyToNull(facebook);
+    if (uri == null) {
+      return null;
+    }
+    return "http://www.facebook.com" + facebook;
   }
 }
