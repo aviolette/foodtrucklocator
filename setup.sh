49edee6fdb5ead22ea765b7f0e350fb1796efa65
Andrew Violette <aviolette@gmail.com>
2012-10-26 10:58:31 -0500

    Add setup script and vendor jars

---
+++ setup.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+APPENGINE_VERSION=1.7.3
+
+mvn install:install-file -Dfile=vendor/google/guice-2.0.jar \
+    -DgroupId=com.google.code.guice \
+    -DartifactId=guice \
+    -Dversion=2.0.1 \
+    -Dpackaging=jar 
+
+mvn install:install-file -Dfile=vendor/google/guice-servlet-2.0.jar  \
+    -DgroupId=com.google.code.guice \
+    -DartifactId=guice-servlet \
+    -Dversion=2.0.1 \
+    -Dpackaging=jar 
+
+mvn install:install-file -Dfile=vendor/google/gdata-client-1.0.jar \
+    -DgroupId=com.google.gdata \
+    -DartifactId=gdata-client \
+    -Dversion=1.0 \
+    -Dpackaging=jar 
+
+mvn install:install-file -Dfile=vendor/google/gdata-core-1.0.jar \
+    -DgroupId=com.google.gdata \
+    -DartifactId=gdata-core \
+    -Dversion=1.0 \
+    -Dpackaging=jar 
+
+mvn install:install-file -Dfile=vendor/google/gdata-calendar-2.0.jar \
+    -DgroupId=com.google.gdata \
+    -DartifactId=gdata-calendar \
+    -Dversion=2.0 \
+    -Dpackaging=jar 
+
+mvn install:install-file -Dfile=$APPENGINE_HOME/lib/shared/appengine-local-runtime-shared.jar \
+    -DgroupId=com.google \
+    -DartifactId=appengine-local-runtime-shared \
+    -Dversion=$APPENGINE_VERSION \
+    -Dpackaging=jar 
+
+mvn install:install-file -Dfile=$APPENGINE_HOME/lib/user/appengine-api-1.0-sdk-$APPENGINE_VERSION.jar \
+    -DgroupId=com.google \
+    -DartifactId=appengine-api-1.0-sdk \
+    -Dpackaging=jar \
+    -Dversion=$APPENGINE_VERSION \
+    
