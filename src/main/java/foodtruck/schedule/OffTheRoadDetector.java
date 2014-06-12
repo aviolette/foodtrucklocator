67ec5e15fe0f69df0655f06b25125f57fdce7061
aviolette <aviolette@gmail.com>
2014-06-12 07:21:37 -0500

    Added 'off the streets' to the off the road detector

--- src/main/java/foodtruck/schedule/OffTheRoadDetector.java
+++ src/main/java/foodtruck/schedule/OffTheRoadDetector.java
@@ -9,7 +9,8 @@ public class OffTheRoadDetector {
   public OffTheRoadResponse offTheRoad(String tweet) {
     String lower = tweet.toLowerCase();
 
-    if (lower.contains("off the road") || lower.contains("in the shop") || lower.contains("at the shop") ||
+    if (lower.contains("off the road") || lower.contains("off the streets") || lower.contains("in the shop") ||
+        lower.contains("at the shop") ||
         lower.contains("no stops today") || lower.contains("no service today") || lower.contains("no truck today") ||
         (lower.contains("cancel") && lower.contains("delivery"))) {
       return new OffTheRoadResponse(true, true);
