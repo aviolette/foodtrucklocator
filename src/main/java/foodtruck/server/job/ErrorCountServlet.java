c074f620cbf823543392052dec4d2e345ed661bd
aviolette <aviolette@gmail.com>
2014-07-02 07:51:34 -0500

    Added some logging for the error servlet

--- src/main/java/foodtruck/server/job/ErrorCountServlet.java
+++ src/main/java/foodtruck/server/job/ErrorCountServlet.java
@@ -1,6 +1,8 @@
 package foodtruck.server.job;
 
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -25,6 +27,7 @@ import foodtruck.util.Clock;
 public class ErrorCountServlet extends HttpServlet {
   private final Clock clock;
   private final FifteenMinuteRollupDAO dao;
+  private static final Logger log = Logger.getLogger(ErrorCountServlet.class.getName());
 
   @Inject
   public ErrorCountServlet(Clock clock, FifteenMinuteRollupDAO dao) {
@@ -39,6 +42,7 @@ public class ErrorCountServlet extends HttpServlet {
     query.endTimeMillis(clock.now().getMillis());
     query.minLogLevel(LogService.LogLevel.ERROR);
     int count = Iterables.size(LogServiceFactory.getLogService().fetch(query));
+    log.log(Level.INFO, "There were {0} errors in the last 15 minutes", count);
     dao.updateCount(clock.now(), "app_error_count", count);
   }
 }
