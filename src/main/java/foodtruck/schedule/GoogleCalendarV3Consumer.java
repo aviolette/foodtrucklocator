da03776bdc7b541a42c9f0121809f0274780f84b
Andrew Violette <aviolette@gmail.com>
2015-06-04 06:47:36 -0500

    Skip TBD

--- src/main/java/foodtruck/schedule/GoogleCalendarV3Consumer.java
+++ src/main/java/foodtruck/schedule/GoogleCalendarV3Consumer.java
@@ -98,7 +98,7 @@ public class GoogleCalendarV3Consumer implements ScheduleStrategy {
           final String titleText = event.getSummary();
           if (!Strings.isNullOrEmpty(titleText)) {
             String lowerTitle = titleText.toLowerCase();
-            if (lowerTitle.contains("private") || lowerTitle.contains("catering")) {
+            if (lowerTitle.contains("private") || lowerTitle.contains("catering") || lowerTitle.contains("TBD")) {
               log.log(Level.INFO, "Skipping {0} for {1}", new Object[]{titleText, truck.getId()});
               continue;
             }
