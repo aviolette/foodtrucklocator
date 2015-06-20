5bbd6e916f512d89d6d6cd8f4492dda0503106c0
Andrew Violette <aviolette@gmail.com>
2015-06-20 07:12:53 -0500

    Copy memcached stats into persistent daily time series data

--- src/main/java/foodtruck/server/job/PurgeStatsServlet.java
+++ src/main/java/foodtruck/server/job/PurgeStatsServlet.java
@@ -1,6 +1,8 @@
 package foodtruck.server.job;
 
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -12,6 +14,7 @@ import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
 import foodtruck.dao.ApplicationDAO;
+import foodtruck.dao.DailyRollupDAO;
 import foodtruck.dao.FifteenMinuteRollupDAO;
 import foodtruck.model.Application;
 import foodtruck.util.Clock;
@@ -22,17 +25,21 @@ import foodtruck.util.Clock;
  */
 @Singleton
 public class PurgeStatsServlet extends HttpServlet {
+  private static final Logger log = Logger.getLogger(PurgeStatsServlet.class.getName());
   private Clock clock;
   private FifteenMinuteRollupDAO dao;
   private MemcacheService memcache;
   private ApplicationDAO appDAO;
+  private DailyRollupDAO dailyDAO;
 
   @Inject
-  public void PurgeStatsServlet(FifteenMinuteRollupDAO dao, Clock clock, MemcacheService service, ApplicationDAO appDAO) {
+  public void PurgeStatsServlet(FifteenMinuteRollupDAO dao, Clock clock, MemcacheService service, ApplicationDAO appDAO,
+      DailyRollupDAO dailyRollupDAO) {
     this.dao = dao;
     this.clock = clock;
     this.memcache = service;
     this.appDAO = appDAO;
+    this.dailyDAO = dailyRollupDAO;
   }
 
   @Override protected void doGet(HttpServletRequest req, HttpServletResponse resp)
@@ -40,7 +47,13 @@ public class PurgeStatsServlet extends HttpServlet {
     dao.deleteBefore(clock.currentDay().minusDays(2));
     // Need to roll these stats up, but for now just cancel
     for (Application application : appDAO.findAll()) {
-      memcache.put("service.access.daily."+application.getKey(), 0L);
+      String key = "service.counts.daily." + application.getKey();
+      try {
+        dailyDAO.updateCount(clock.now().minusHours(1), key, (Long)memcache.get(key));
+      } catch (Exception e) {
+        log.log(Level.WARNING, e.getMessage(), e);
+      }
+      memcache.put(key, 0L);
     }
   }
 }
