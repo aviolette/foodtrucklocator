8bf0bd7148d0010b3f7b878d710bb3bb740dacdc
Andrew Violette <aviolette@gmail.com>
2016-05-15 19:00:48 -0500

    Support SSL challenge response.

--- src/main/java/foodtruck/server/SSLVerificationServlet.java
+++ src/main/java/foodtruck/server/SSLVerificationServlet.java
@@ -17,6 +17,6 @@ import com.google.inject.Singleton;
 public class SSLVerificationServlet extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    resp.getWriter().write("dswFbgBMCKQDsjmSJqFe0pRNtTfbrx7VEskhnkFcoFM.G0-9lfhFfooxK32XNFfifqqxYRWjUgHiFf32xVhm8zo");
+    resp.getWriter().write(System.getProperty("challenge.response", ""));
   }
 }
