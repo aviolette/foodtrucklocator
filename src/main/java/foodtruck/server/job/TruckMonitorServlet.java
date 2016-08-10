081074d9f36ee1cce16fb20d097caf6e518beda7
Andrew Violette <aviolette@gmail.com>
2016-08-10 07:34:38 -0500

    Added ability to refresh the beacon servlet

--- src/main/java/foodtruck/server/job/TruckMonitorServlet.java
+++ src/main/java/foodtruck/server/job/TruckMonitorServlet.java
@@ -1,12 +1,14 @@
 package foodtruck.server.job;
 
 import java.io.IOException;
+import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.common.base.Strings;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
@@ -20,6 +22,8 @@ import foodtruck.linxup.TruckMonitorService;
  */
 @Singleton
 public class TruckMonitorServlet extends HttpServlet {
+  private static final Logger log = Logger.getLogger(TruckMonitorServlet.class.getName());
+
   private final TruckMonitorService service;
 
   @Inject
@@ -29,6 +33,13 @@ public class TruckMonitorServlet extends HttpServlet {
 
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    log.info("Synchronizing monitoring devices...");
     this.service.synchronize();
+    String refreshValue = req.getParameter("refresh");
+    if (Strings.isNullOrEmpty(refreshValue)) {
+      return;
+    }
+    resp.setHeader("Content-Type", "text/html");
+    resp.getOutputStream().println("<html><head><meta http-equiv=\"refresh\" content=\"" + refreshValue + "\"></head></html>");
   }
 }
