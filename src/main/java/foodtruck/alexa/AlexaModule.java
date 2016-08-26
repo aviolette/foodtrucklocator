b6814a42924745e80d5498f7db0f8ae60f32e985
Andrew Violette <aviolette@gmail.com>
2016-08-25 20:22:57 -0500

    Added TruckLocationIntentProcessor

--- src/main/java/foodtruck/alexa/AlexaModule.java
+++ src/main/java/foodtruck/alexa/AlexaModule.java
@@ -11,6 +11,7 @@ import com.google.inject.multibindings.MapBinder;
  */
 public class AlexaModule extends AbstractModule {
   static final String GET_FOOD_TRUCKS_AT_LOCATION = "GetFoodTrucksAtLocation";
+  static final String WHERE_IS_TRUCK = "WhereIsTruck";
 
   @Override
   protected void configure() {
@@ -18,5 +19,6 @@ public class AlexaModule extends AbstractModule {
     MapBinder<String, IntentProcessor> intentProcessorMapBinder = MapBinder.newMapBinder(binder(), String.class,
         IntentProcessor.class);
     intentProcessorMapBinder.addBinding(GET_FOOD_TRUCKS_AT_LOCATION).to(LocationIntentProcessor.class);
+    intentProcessorMapBinder.addBinding(WHERE_IS_TRUCK).to(TruckLocationInentProcessor.class);
   }
 }
