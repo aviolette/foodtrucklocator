c0f120c906f84ddc4b9d705802439cb9209d9b7a
Andrew Violette <aviolette@gmail.com>
2016-08-30 08:17:26 -0500

    Added better logging to speechlet and added welcome message

--- src/main/java/foodtruck/alexa/AlexaUtils.java
+++ src/main/java/foodtruck/alexa/AlexaUtils.java
@@ -2,7 +2,12 @@ package foodtruck.alexa;
 
 import java.util.List;
 
+import com.amazon.speech.slu.Intent;
+import com.amazon.speech.slu.Slot;
+import com.google.common.base.Function;
 import com.google.common.base.Joiner;
+import com.google.common.base.MoreObjects;
+import com.google.common.collect.FluentIterable;
 
 /**
  * @author aviolette
@@ -22,5 +27,21 @@ class AlexaUtils {
     String rdc = JOINER.join(list.subList(0, list.size() - 1));
     String oxfordComma = (list.size() == 2) ? "" : ",";
     return rdc + oxfordComma + " and " + rac;
+  }
+
+  static String intentToString(Intent intent) {
+    return MoreObjects.toStringHelper(intent)
+        .add("name", intent.getName())
+        .add("slots", FluentIterable.from(intent.getSlots().values()).transform(new Function<Slot, String>() {
+          @Override
+          public String apply(Slot input) {
+            return MoreObjects.toStringHelper(input)
+                .add("slot", input.getName())
+                .add("value", input.getValue())
+                .toString();
+          }
+        }).join(Joiner.on("\n")))
+        .toString();
+
   }
 }
--- src/main/java/foodtruck/alexa/FTFSpeechlet.java
+++ src/main/java/foodtruck/alexa/FTFSpeechlet.java
@@ -1,6 +1,8 @@
 package foodtruck.alexa;
 
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import com.amazon.speech.speechlet.IntentRequest;
 import com.amazon.speech.speechlet.LaunchRequest;
@@ -17,6 +19,8 @@ import com.google.inject.Inject;
  * @since 8/25/16
  */
 class FTFSpeechlet implements Speechlet {
+  private static final Logger log = Logger.getLogger(FTFSpeechlet.class.getName());
+
   private final Map<String, IntentProcessor> processors;
 
   @Inject
@@ -26,23 +30,31 @@ class FTFSpeechlet implements Speechlet {
 
   @Override
   public void onSessionStarted(SessionStartedRequest sessionStartedRequest, Session session) throws SpeechletException {
-
+    log.info("Alexa session started: " + session.getSessionId());
   }
 
   @Override
   public SpeechletResponse onLaunch(LaunchRequest launchRequest, Session session) throws SpeechletException {
-    return null;
+    return SpeechletResponseBuilder.builder()
+        .speechText("Food Truck Finder.  What would you like to find?")
+        .repromptText("With Food Truck Finder you can find out where individual food trucks are, or find out what " +
+            "food trucks will be at a specific location.  For example, you can say What food trucks " +
+            "are on Wacker today.  What location would you like to know about?")
+        .ask();
   }
 
   @Override
   public SpeechletResponse onIntent(IntentRequest intentRequest, Session session) throws SpeechletException {
+    log.log(Level.INFO, "Received intent {} ", AlexaUtils.intentToString(intentRequest.getIntent()));
     IntentProcessor processor = processors.get(intentRequest.getIntent().getName());
-    // handle null
+    if (processor == null) {
+      throw new SpeechletException("Invalid intent: " + intentRequest.getIntent().getName());
+    }
     return processor.process(intentRequest.getIntent(), session);
   }
 
   @Override
   public void onSessionEnded(SessionEndedRequest sessionEndedRequest, Session session) throws SpeechletException {
-
+    log.fine("Alexa session ended:" + session.getSessionId());
   }
 }
