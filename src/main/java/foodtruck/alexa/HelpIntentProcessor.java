dc340aa3b32f7e831541f3b7b1f78ffc6c59c60e
Andrew Violette <aviolette@gmail.com>
2016-09-09 21:15:25 -0500

    Added some more help

--- src/main/java/foodtruck/alexa/HelpIntentProcessor.java
+++ src/main/java/foodtruck/alexa/HelpIntentProcessor.java
@@ -17,7 +17,7 @@ class HelpIntentProcessor implements IntentProcessor {
         .repromptText(
             "Food Truck Finder.  You can ask me what trucks will be at a location or information on a specific truck.  For example, you can say What trucks are on Clark and Monroe?  Or Where will Cheesies be for lunch today?")
         .simpleCardWithText("Food Truck Finder Help",
-            "Some example things to say:\n What food trucks are at Clark and Monroe today?\n Tell me about Cheesies.\n Where will Corner Farmacy be for lunch?")
+            "Some example things to say:\n What food trucks are at Clark and Monroe today?\n Tell me about Cheesies.\n Where will Corner Farmacy be for lunch?\nWhat are the Doughnut Vault's specials?")
         .ask();
   }
 }
