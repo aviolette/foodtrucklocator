e1ed3581f5cce4a5c037fc9379159e83fb1b51c6
Andrew Violette <aviolette@gmail.com>
2016-09-13 16:54:02 -0500

    Added logging to alexa servlet

--- src/main/java/foodtruck/server/AlexaServlet.java
+++ src/main/java/foodtruck/server/AlexaServlet.java
@@ -1,5 +1,11 @@
 package foodtruck.server;
 
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import com.amazon.speech.speechlet.Speechlet;
 import com.amazon.speech.speechlet.servlet.SpeechletServlet;
 import com.google.inject.Inject;
@@ -11,10 +17,17 @@ import com.google.inject.Singleton;
  */
 @Singleton
 public class AlexaServlet extends SpeechletServlet {
+  private static final Logger log = Logger.getLogger(AlexaServlet.class.getName());
 
   @Inject
   public AlexaServlet(Speechlet alexaSpeechlet) {
     super();
     setSpeechlet(alexaSpeechlet);
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    log.info("Alexa request received");
+    super.doPost(request, response);
   }
 }
