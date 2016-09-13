e6b2c9640f63a7fafb42b7ae938eab168f6c1837
Andrew Violette <aviolette@gmail.com>
2016-09-12 20:12:57 -0500

    Tweaked the copy of help messages

--- src/main/java/foodtruck/alexa/CategoryIntent.java
+++ src/main/java/foodtruck/alexa/CategoryIntent.java
@@ -5,14 +5,16 @@ import java.util.List;
 import com.amazon.speech.slu.Intent;
 import com.amazon.speech.speechlet.Session;
 import com.amazon.speech.speechlet.SpeechletResponse;
+import com.google.common.base.Strings;
 import com.google.common.collect.FluentIterable;
+import com.google.common.collect.ImmutableList;
 import com.google.inject.Inject;
 
 import foodtruck.dao.TruckDAO;
 import foodtruck.model.Truck;
-import foodtruck.util.MoreStrings;
 
 import static foodtruck.alexa.Conjunction.and;
+import static foodtruck.util.MoreStrings.capitalize;
 
 /**
  * @author aviolette
@@ -21,6 +23,8 @@ import static foodtruck.alexa.Conjunction.and;
 public class CategoryIntent implements IntentProcessor {
 
   private static final String CATEGORY_NAME = "Category";
+  private static final ImmutableList<String> NATIONALITIES = ImmutableList.of("asian", "mexican", "cajun", "german",
+      "greek", "jamaican", "korean", "polish", "venezuelan", "vietnamese");
   private final TruckDAO truckDAO;
 
   @Inject
@@ -32,19 +36,23 @@ public class CategoryIntent implements IntentProcessor {
   public SpeechletResponse process(Intent intent, Session session) {
     String category = intent.getSlot(CATEGORY_NAME)
         .getValue();
-    if (category == null) {
-      // TODO: help
+    if (Strings.isNullOrEmpty(category)) {
       return SpeechletResponseBuilder.builder()
-          .speechText("You can find trucks by category.  What category would you like to search for?")
-          .repromptText("For example, you can say 'What trucks have tacos?'.  What would you like to find?")
+          .speechText("What keyword would you like to search for?")
+          .repromptText(
+              "You can search for food trucks by what type of food they sell.  For example, you can say 'What trucks sell tacos?'.  What kind of food do you want to find?")
           .ask();
     }
     List<String> trucks = FluentIterable.from(truckDAO.findActiveTrucks())
-        .filter(new Truck.HasCategoryPredicate(MoreStrings.capitalize(category)))
+        .filter(new Truck.HasCategoryPredicate(capitalize(category)))
         .transform(Truck.TO_NAME)
         .toList();
+    if (NATIONALITIES.contains(category.toLowerCase())) {
+      category = capitalize(category) + " food";
+    }
     String result = trucks.isEmpty() ? "There are no trucks that have " + category : String.format(
         "These trucks have %s: %s", category, AlexaUtils.toAlexaList(trucks, true, and));
+
     return SpeechletResponseBuilder.builder()
         .speechSSML(result)
         .simpleCard("Trucks that have " + category)
--- src/main/java/foodtruck/alexa/HelpIntentProcessor.java
+++ src/main/java/foodtruck/alexa/HelpIntentProcessor.java
@@ -15,7 +15,7 @@ class HelpIntentProcessor implements IntentProcessor {
         .speechText(
             "Food Truck Finder.  You can ask me what trucks will be at a location or information on a specific truck.")
         .repromptText(
-            "Food Truck Finder.  You can ask me what trucks will be at a location or information on a specific truck.  For example, you can say What trucks are on Clark and Monroe?  Or Where will Cheesies be for lunch today?")
+            "Food Truck Finder.  You can ask me what trucks will be at a location or information on a specific truck.  For example, you can say What trucks are on Clark and Monroe?  Or Where will Cheesies be for lunch today?  What would you like to find?")
         .simpleCardWithText("Food Truck Finder Help",
             "Some example things to say:\n\nWhat food trucks are at Clark and Monroe today?\n\nTell me about Cheesies.\n\nWhere will Corner Farmacy be for lunch?\n\nWhat are the Doughnut Vault's specials?")
         .ask();
