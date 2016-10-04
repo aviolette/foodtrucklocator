ad20eeadcf983c108245443cbd2bb183de786b88
Andrew Violette <aviolette@gmail.com>
2016-10-04 06:19:06 -0500

    Helper app to find slugs for twitter list entries

---
+++ src/main/java/foodtruck/notifications/ListList.java
@@ -0,0 +1,41 @@
+package foodtruck.notifications;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import com.google.common.base.Throwables;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.UserList;
+import twitter4j.conf.PropertyConfiguration;
+
+/**
+ * An app to find the slugs for the twitter lists
+ *
+ * @author aviolette
+ * @since 10/3/16
+ */
+@SuppressWarnings("AppEngineForbiddenCode")
+public class ListList {
+  public static void main(String args[]) throws Exception {
+    Properties properties = new Properties();
+    InputStream in = Thread.currentThread()
+        .getContextClassLoader()
+        .getResourceAsStream("twitter4j.properties");
+    try {
+      properties.load(in);
+      in.close();
+    } catch (IOException e) {
+      throw Throwables.propagate(e);
+    }
+    Twitter twitter = new TwitterFactory(new PropertyConfiguration(properties)).getInstance();
+    for (UserList userList : twitter.list()
+        .getUserLists("chifoodtruckz")) {
+      System.out.println(userList.getName() + " " + userList.getSlug());
+    }
+    System.exit(0);
+  }
+
+}
