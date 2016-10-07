91581eba279a87777259a21841d36ac30ea89642
Andrew Violette <aviolette@gmail.com>
2016-10-06 20:28:58 -0500

    Added GorillaCheeseNYC matcher

---
+++ src/main/java/foodtruck/schedule/custom/nyc/GorillaCheeseNYCMatcher.java
@@ -0,0 +1,24 @@
+package foodtruck.schedule.custom.nyc;
+
+import com.google.common.collect.ImmutableList;
+import com.google.inject.Inject;
+
+import org.joda.time.format.DateTimeFormatter;
+
+import foodtruck.geolocation.GeoLocator;
+import foodtruck.schedule.MultipleTruckMatcher;
+import foodtruck.schedule.Spot;
+import foodtruck.util.Clock;
+import foodtruck.util.FriendlyDateTimeFormat;
+
+/**
+ * @author aviolette
+ * @since 10/6/16
+ */
+public class GorillaCheeseNYCMatcher extends MultipleTruckMatcher {
+  @Inject
+  public GorillaCheeseNYCMatcher(GeoLocator geoLocator, ImmutableList<Spot> commonSpots,
+      @FriendlyDateTimeFormat DateTimeFormatter formatter, Clock clock) {
+    super(geoLocator, commonSpots, formatter, clock);
+  }
+}
--- src/main/java/foodtruck/schedule/custom/nyc/NewYorkModule.java
+++ src/main/java/foodtruck/schedule/custom/nyc/NewYorkModule.java
@@ -24,12 +24,19 @@ public class NewYorkModule extends AbstractModule {
     MapBinder<String, SpecialMatcher> binder = MapBinder.newMapBinder(binder(), String.class, SpecialMatcher.class);
     binder.addBinding("usafoodtruck")
         .to(USATruckMatcher.class);
+    binder.addBinding("gcnyc1")
+        .to(GorillaCheeseNYCMatcher.class);
   }
 
   @Provides
   @Singleton
   public ImmutableList<Spot> provideCommonSpots() {
     return ImmutableList.of(new Spot("old slip/water", "Old Slip and Water, New York, NY"), new Spot("dumbo", "DUMBO"),
+        new Spot("water/jay", "Water and Jay, New York, NY"), new Spot("park/33", "Park and 33rd, New York, NY"),
+        new Spot("91/columbus", "91th and Columbus, New York, NY"),
+        new Spot("broadway/55", "Broadway and 55th, New York, NY"),
+        new Spot("madison/98", "Madison and 98th, New York, NY"),
+        new Spot("madison/58", "Madison and 58th, New York, NY"),
         new Spot("49th st in between 6th and 7th ave", "49th and 6th, New York, NY"),
         new Spot("30-30 47th ave", "47th Ave and 30th St, Long Island City, NY"),
         new Spot("hudson/king", "Hudson and King, New York, NY"), new Spot("metrotech", "metrotech"));
