6cdd4560cbe995ead9b66150200bfca920e71e1a
Andrew Violette <aviolette@gmail.com>
2016-11-21 16:00:47 -0600

    Send stats via queue

--- main/src/main/java/foodtruck/monitoring/MonitorInterceptor.java
+++ main/src/main/java/foodtruck/monitoring/MonitorInterceptor.java
@@ -1,10 +1,12 @@
 package foodtruck.monitoring;
 
+import com.google.appengine.api.taskqueue.Queue;
+import com.google.appengine.api.taskqueue.TaskOptions;
 import com.google.inject.Inject;
+import com.google.inject.Provider;
 
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
-import org.joda.time.DateTime;
 
 import foodtruck.dao.FifteenMinuteRollupDAO;
 import foodtruck.util.Clock;
@@ -16,30 +18,34 @@ import foodtruck.util.Clock;
 public class MonitorInterceptor implements MethodInterceptor {
   private FifteenMinuteRollupDAO fifteenMinuteRollupDAO;
   private Clock clock;
+  private Provider<Queue> monitorQueueProvider;
 
   @Inject
   public MonitorInterceptor() {
   }
 
   @Inject
-  public void initialize(FifteenMinuteRollupDAO dao, Clock clock) {
+  public void initialize(FifteenMinuteRollupDAO dao, Clock clock, Provider<Queue> monitorQueue) {
     this.fifteenMinuteRollupDAO = dao;
     this.clock = clock;
+    this.monitorQueueProvider = monitorQueue;
   }
 
   @Override public Object invoke(MethodInvocation invocation) throws Throwable {
     String methodName = invocation.getMethod().getName();
     String className = invocation.getMethod().getDeclaringClass().getName();
-    // TODO: This is not thread-safe!
     String prefix = className + "_" + methodName;
-    DateTime now = clock.now();
+    Queue queue = monitorQueueProvider.get();
     try {
+      System.out.println("HERE");
       return invocation.proceed();
     } catch (Exception e) {
-      fifteenMinuteRollupDAO.updateCount(now, prefix + "_failed");
+      queue.add(TaskOptions.Builder.withUrl("/cron/update_count")
+          .param("statName", prefix + "_failed"));
       throw e;
     } finally {
-      fifteenMinuteRollupDAO.updateCount(now, prefix + "_total");
+      queue.add(TaskOptions.Builder.withUrl("/cron/update_count")
+          .param("statName", prefix + "total"));
     }
   }
 }
