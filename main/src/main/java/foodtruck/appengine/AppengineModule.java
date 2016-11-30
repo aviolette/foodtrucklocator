bb78984a05ca3fa9e163689595f2368d745ed739
Andrew Violette <aviolette@gmail.com>
2016-11-30 07:59:34 -0600

    Appengine module is no longer private

--- main/src/main/java/foodtruck/appengine/AppengineModule.java
+++ main/src/main/java/foodtruck/appengine/AppengineModule.java
@@ -11,21 +11,19 @@ import com.google.appengine.api.taskqueue.Queue;
 import com.google.appengine.api.taskqueue.QueueFactory;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
-import com.google.inject.Exposed;
-import com.google.inject.PrivateModule;
+import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 
 /**
  * @author aviolette
  * @since 11/23/16
  */
-public class AppengineModule extends PrivateModule {
+public class AppengineModule extends AbstractModule {
   @Override
   protected void configure() {
   }
 
   @Provides
-  @Exposed
   public MemcacheService provideMemcacheService() {
     MemcacheService syncCache = MemcacheServiceFactory.getMemcacheService();
     syncCache.setErrorHandler(ErrorHandlers.getConsistentLogAndContinue(Level.INFO));
@@ -33,19 +31,16 @@ public class AppengineModule extends PrivateModule {
   }
 
   @Provides
-  @Exposed
   public UserService provideUserService() {
     return UserServiceFactory.getUserService();
   }
 
   @Provides
-  @Exposed
   public Queue provideQueue() {
     return QueueFactory.getDefaultQueue();
   }
 
   @Provides
-  @Exposed
   public DatastoreService provideDatastoreService() {
     return DatastoreServiceFactory.getDatastoreService();
   }
