022418d021ea881ea2b7f31a631b54126b7dc145
Andrew Violette <aviolette@gmail.com>
2016-11-30 06:51:23 -0600

    Added yet more logging for location resolver

--- main/src/main/java/foodtruck/linxup/LocationResolverImpl.java
+++ main/src/main/java/foodtruck/linxup/LocationResolverImpl.java
@@ -86,6 +86,9 @@ class LocationResolverImpl implements LocationResolver {
             .plusMinutes(59)
             .toDate()));
         return true;
+      } else {
+        log.log(Level.INFO, "Anomaly not detected\n\n {0}\n\n {1}\n\n",
+            new Object[]{stop.getLocation(), currentLocation});
       }
     } catch (Exception e) {
       log.log(Level.SEVERE, e.getMessage(), e);
