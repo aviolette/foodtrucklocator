004901b00446c754d69ec69f721b10e4882ba18e
Andrew Violette <aviolette@gmail.com>
2016-11-30 20:28:29 -0600

    Started working on foodtruckfinder stand-alone service

---
+++ ftf/foodtruckfinder/src/main/java/foodtruck/finder/server/FinderServletModule.java
@@ -0,0 +1,18 @@
+package foodtruck.finder.server;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.inject.servlet.ServletModule;
+import com.sun.jersey.guice.spi.container.servlet.GuiceContainer;
+import com.sun.jersey.api.core.PackagesResourceConfig;
+
+/**
+ * @author aviolette
+ * @since 10/12/16
+ */
+public class FinderServletModule extends ServletModule {
+  @Override
+  protected void configureServlets() {
+    serve("/v1/services/*").with(GuiceContainer.class,
+        ImmutableMap.of(PackagesResourceConfig.PROPERTY_PACKAGES, "foodtruck.finder.resources,foodtruck.json.jettison"));
+  }
+}
