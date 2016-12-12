df88d2b054b78ea6c05d5e1dbfbc93901fb678ba
Andrew Violette <aviolette@gmail.com>
2016-12-12 16:53:33 -0600

    Moved SimpleEmailNotifierTest to core-services test

--- ftf/core-services/src/test/java/foodtruck/mail/SimpleEmailNotifierTest.java
+++ ftf/core-services/src/test/java/foodtruck/mail/SimpleEmailNotifierTest.java
@@ -2,43 +2,45 @@ package foodtruck.mail;
 
 import com.google.common.collect.ImmutableSet;
 
-import org.easymock.EasyMockSupport;
+
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import foodtruck.model.Location;
 import foodtruck.model.StaticConfig;
 import foodtruck.model.Truck;
 import foodtruck.model.TruckStop;
 
-import static org.easymock.EasyMock.expect;
+import static org.mockito.Mockito.when;
 
 /**
  * @author aviolette
  * @since 8/13/15
  */
-public class SimpleEmailNotifierTest extends EasyMockSupport {
+@RunWith(MockitoJUnitRunner.class)
+public class SimpleEmailNotifierTest {
 
   private SimpleEmailNotifier notifier;
-  private StaticConfig config;
-  private EmailSender sender;
+  @Mock private StaticConfig config;
+  @Mock private EmailSender sender;
 
   @Before
   public void before() {
     DateTimeZone zone = DateTimeZone.forID("America/Chicago");
     DateTimeFormatter timeFormatter = DateTimeFormat.forPattern("YYYYMMdd-HHmm").withZone(zone);
-    config = createMock(StaticConfig.class);
-    sender = createMock(EmailSender.class);
     notifier = new SimpleEmailNotifier(config, timeFormatter, sender);
   }
 
   @Test
   public void testNotifyAddMentionedTrucks() throws Exception {
-    expect(config.getBaseUrl()).andReturn("http://localhost");
+    when(config.getBaseUrl()).thenReturn("http://localhost");
     TruckStop stop = TruckStop.builder()
         .truck(Truck.builder().id("foo").name("bar").build())
         .startTime(new DateTime(2015, 8, 13, 12, 0, DateTimeZone.UTC))
@@ -48,8 +50,6 @@ public class SimpleEmailNotifierTest extends EasyMockSupport {
     sender.sendSystemMessage("Truck was mentioned by another truck", "This tweet \"foobar\"\n" +
         "\n from bar might have indicated that there additional trucks to be added to the system.\n\n  " +
         "Click here http://localhost/admin/event_at/123?selected=truck1,truck2&startTime=20150813-0700&endTime=20150812-2100 to add the trucks");
-    replayAll();
     notifier.notifyAddMentionedTrucks(ImmutableSet.of("truck1", "truck2"), stop, "foobar");
-    verifyAll();
   }
 }
