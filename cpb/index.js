7b15ab9f5db7642a85de2532eda4d7fb34d816d1
Andrew Violette <aviolette@gmail.com>
2018-11-20 08:52:59 -0600

    Code used in google cloud function used to extract chicago pizza boss links

---
+++ cpb/index.js
@@ -0,0 +1,54 @@
+const puppeteer = require('puppeteer');
+let page;
+
+/*
+const Storage = require('@google-cloud/storage');
+
+const storage = Storage({
+  projectId: 'chicagofoodtrucklocator'
+});
+
+const bucket = storage.bucket('chicagopizzaboss');
+*/
+
+async function getBrowserPage() {
+  // Launch headless Chrome. Turn off sandbox so Chrome can run under root.
+  const browser = await puppeteer.launch({args: ['--no-sandbox']});
+  return browser.newPage();
+}
+
+/**
+ * Responds to any HTTP request.
+ *
+ * @param {!express:Request} req HTTP request context.
+ * @param {!express:Response} res HTTP response context.
+ */
+exports.schedule = async (req, res) => {
+  
+  if (!page) {
+    page = await getBrowserPage();
+  }
+
+  await page.goto('http://chicagopizzaboss.com/new-events/', {waitUntil: 'networkidle2'});
+  const hrefs  = await page.evaluate(() => {
+      const list = document.querySelectorAll("a");
+      const hrefs = [];
+      const hrefHash = {};
+      for (const element of list) {
+        if (/new-events\/2/.exec(element.href)) {
+          if (!hrefHash[element.href]) {
+            hrefs.push(element.href + "?format=ical");
+            hrefHash[element.href] = 1;
+          }
+        }
+      }
+      return hrefs;
+  });
+  /*
+  bucket.file("calendar2.json").createWriteStream()
+    .on('error', function(err) {})
+    .on('finish', function() {})
+    .end(JSON.stringify(hrefs)); 
+    */
+  res.status(200).send(hrefs)
+};
