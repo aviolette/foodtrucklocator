c57a80ef4f0e3da58813b9ca95bfbab1b6b2d743
Andrew Violette <aviolette@gmail.com>
2018-11-20 12:55:26 -0600

    Changed pizza boss code to hit cloud function

--- main/src/main/java/foodtruck/schedule/PizzaBossSuperSpecialConsumer.java
+++ main/src/main/java/foodtruck/schedule/PizzaBossSuperSpecialConsumer.java
@@ -1,20 +1,16 @@
 package foodtruck.schedule;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.channels.Channels;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.annotation.Nullable;
 
-import com.google.appengine.tools.cloudstorage.GcsFilename;
-import com.google.appengine.tools.cloudstorage.GcsInputChannel;
-import com.google.appengine.tools.cloudstorage.GcsService;
 import com.google.common.collect.ImmutableList;
-import com.google.common.io.ByteStreams;
 import com.google.inject.Inject;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientHandlerException;
+import com.sun.jersey.api.client.UniformInterfaceException;
 
 import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONException;
@@ -29,46 +25,45 @@ import foodtruck.server.resources.json.JSONSerializer;
  * @author aviolette
  * @since 11/13/18
  */
-@SuppressWarnings("UnstableApiUsage")
 public class PizzaBossSuperSpecialConsumer implements ScheduleStrategy {
 
   private static final Logger log = Logger.getLogger(PizzaBossSuperSpecialConsumer.class.getName());
 
-  private final GcsService cloudStorage;
   private final ICalStopConsumer consumer;
   private final TruckDAO truckDAO;
+  private final Client client;
 
   @Inject
-  public PizzaBossSuperSpecialConsumer(GcsService cloudStorage, ICalStopConsumer iCalStopConsumer, TruckDAO truckDAO) {
-    this.cloudStorage = cloudStorage;
+  public PizzaBossSuperSpecialConsumer(ICalStopConsumer iCalStopConsumer, TruckDAO truckDAO, Client client) {
     this.consumer = iCalStopConsumer;
     this.truckDAO = truckDAO;
+    this.client = client;
   }
 
   @Override
   public List<TruckStop> findForTime(Interval range, @Nullable Truck searchTruck) {
-    if (searchTruck != null && !searchTruck.getId().equals("chipizzaboss")) {
+    if (searchTruck != null && !searchTruck.getId()
+        .equals("chipizzaboss")) {
       return ImmutableList.of();
     } else if (searchTruck == null) {
       searchTruck = truckDAO.findByIdOpt("chipizzaboss")
           .orElseThrow(() -> new RuntimeException("Chicago pizza boss not found"));
     }
     log.info("Loading pizza bosses schedule...");
-    GcsFilename fileName = new GcsFilename("chicagopizzaboss", "calendar.json");
-    GcsInputChannel readChannel = cloudStorage.openPrefetchingReadChannel(fileName, 0, 2097152);
-    try (InputStream in = Channels.newInputStream(readChannel)) {
-      String json = new String(ByteStreams.toByteArray(in), "UTF-8");
-      JSONArray arr = new JSONArray(json);
-      ImmutableList.Builder<TruckStop> stopBuilder = ImmutableList.builder();
-      for (String link : JSONSerializer.toStringList(arr)) {
-        log.log(Level.INFO, "Loading link {0}", link);
-        stopBuilder.addAll(consumer.findForRange(range, searchTruck, link));
+    ImmutableList.Builder<TruckStop> stopBuilder = ImmutableList.builder();
+    for (int i=0; i < 3; i++) {
+      try {
+        JSONArray arr = client.resource(
+            "https://us-central1-chicagofoodtrucklocator.cloudfunctions.net/chicagopizzaboss-schedule")
+            .get(JSONArray.class);
+        for (String link : JSONSerializer.toStringList(arr)) {
+          log.log(Level.INFO, "Loading link {0}", link);
+          stopBuilder.addAll(consumer.findForRange(range, searchTruck, link));
+        }
+      } catch (JSONException | UniformInterfaceException | ClientHandlerException e) {
+        log.log(Level.WARNING, e.getMessage(), e);
       }
-      return stopBuilder.build();
-
-    } catch (IOException | JSONException e) {
-      log.log(Level.WARNING, e.getMessage(), e);
     }
-    return null;
+    return stopBuilder.build();
   }
 }
