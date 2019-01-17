8ccdaea81de824b09b84ebe1e69edb602beda50e
Andrew Violette <aviolette@gmail.com>
2019-01-17 08:27:36 -0600

    Removed cruft from recache servlet

--- main/src/main/java/foodtruck/server/job/RecacheServlet.java
+++ main/src/main/java/foodtruck/server/job/RecacheServlet.java
@@ -1,7 +1,6 @@
 package foodtruck.server.job;
 
 import java.io.IOException;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
@@ -9,19 +8,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.common.base.Strings;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
 import org.joda.time.DateTimeZone;
 import org.joda.time.Interval;
 import org.joda.time.LocalDate;
-import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.DateTimeFormatter;
 
 import foodtruck.dao.RetweetsDAO;
-import foodtruck.dao.TruckDAO;
-import foodtruck.model.StaticConfig;
 import foodtruck.schedule.FoodTruckStopService;
 import foodtruck.schedule.SocialMediaCacher;
 import foodtruck.time.Clock;
@@ -37,57 +31,25 @@ public class RecacheServlet extends HttpServlet {
   private final FoodTruckStopService service;
   private final Clock clock;
   private final SocialMediaCacher socialMediaCacher;
-  private final TruckDAO truckDAO;
-  private final DateTimeFormatter timeFormatter;
-  private final DateTimeZone zone;
   private final RetweetsDAO retweetsDAO;
-  private final StaticConfig staticConfig;
 
   @Inject
-  public RecacheServlet(FoodTruckStopService service, Clock clock, StaticConfig staticConfig,
-      SocialMediaCacher socialMediaCacher, TruckDAO truckDAO, DateTimeZone zone, RetweetsDAO retweetsDAO) {
+  public RecacheServlet(FoodTruckStopService service, Clock clock, SocialMediaCacher socialMediaCacher, RetweetsDAO retweetsDAO) {
     this.socialMediaCacher = socialMediaCacher;
     this.service = service;
     this.clock = clock;
-    this.truckDAO = truckDAO;
-    this.timeFormatter = DateTimeFormat.forPattern("YYYYMMdd").withZone(zone);
-    this.zone = zone;
     this.retweetsDAO = retweetsDAO;
-    this.staticConfig = staticConfig;
   }
 
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
-    final String truck = req.getParameter("truck");
-    final String date = req.getParameter("date");
-
-    LocalDate when = parseDate(date);
+    LocalDate when = clock.currentDay();
+    DateTimeZone zone = clock.zone();
     final Interval instant = when.toInterval(zone).withEnd(when.plusDays(365).toDateTimeAtStartOfDay(zone));
-    if (!Strings.isNullOrEmpty(truck)) {
-      log.info("Recaching truck: " + truck);
-      service.pullCustomCalendarFor(instant, truckDAO.findById(truck));
-    } else {
-      if (staticConfig.isRecachingEnabled()) {
-        log.info("Recaching all trucks");
-        service.pullCustomCalendars(instant);
-      } else {
-        log.info("Recaching disabled");
-      }
-    }
+    log.info("Recaching all trucks");
+    service.pullCustomCalendars(instant);
     socialMediaCacher.analyze();
     retweetsDAO.deleteAll();
-  }
-
-  private LocalDate parseDate(String date) throws ServletException {
-    if (!Strings.isNullOrEmpty(date)) {
-      try {
-        return timeFormatter.parseDateTime(date).withTimeAtStartOfDay().toLocalDate();
-      } catch (Exception e) {
-        log.log(Level.WARNING, "Error formatting time", e);
-        throw new ServletException(e);
-      }
-    }
-    return clock.currentDay();
   }
 }
