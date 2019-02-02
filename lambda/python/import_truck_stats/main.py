516038cb452097e4147053424ca3a22fae50a7f6
Andrew Violette <aviolette@gmail.com>
2019-02-02 09:20:07 -0600

    Initial working version of import_truck_stats cloud function

--- lambda/python/import_truck_stats/main.py
+++ lambda/python/import_truck_stats/main.py
@@ -1,6 +1,7 @@
 import logging
 import json
 from google.cloud import storage
+from google.cloud import datastore
 
 
 def update_data(event, context):
@@ -10,7 +11,6 @@ def update_data(event, context):
          context (google.cloud.functions.Context): Metadata for the event.
     """
     file = event
-    logging.info(file)
 
     if file['name'] == 'stopcounts.json':
         update_truck_counts()
@@ -28,9 +28,20 @@ def retrieve_file(file_name):
 
 def update_truck_counts():
     contents = retrieve_file("stopcounts.json")
+    datastore_client = datastore.Client()
+    kind = 'Store'
     for count in contents:
-        print(f"{count['truckId']}")
-    pass
+        key = datastore_client.key(kind, count['truckId'])
+        truck = datastore_client.get(key)
+        if truck is not None:
+            try:
+                logging.info('Retrieved {}'.format(count['truckId']))
+                truck['all_stops'] = int(count['f0_'])
+                datastore_client.put(truck)
+            except TypeError:
+                logging.error("An error occurred")
+        else:
+            logging.error(f"Could not retrieve {count['truckId']}")
 
 
 if __name__ == "__main__":
