f0a5015349774994aef550843a5b55b4f36edfd3
Andrew Violette <aviolette@divvydose.com>
2019-03-03 11:16:03 -0600

    Fixed run.sh script for RoyalPalms to be more robust

--- lambda/royal-palms/run.sh
+++ lambda/royal-palms/run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-cd /Users/andrewviolette/dev/foodtrucklocator/lambda/royal-palms
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd $DIR
 
 node extractLinks.js > /tmp/royalpalms.json
 gsutil cp /tmp/royalpalms.json gs://cftf_schedule
