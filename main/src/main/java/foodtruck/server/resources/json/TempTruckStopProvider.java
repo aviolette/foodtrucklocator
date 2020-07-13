8767faddefe9602eae1061cd1eb403f586b6ef23
Andrew Violette <aviolette@divvydose.com>
2020-07-13 05:41:22 -0500

    Added geolocation verification step to temp truck stop.   Also, removed address lookup endpoint.

--- main/src/main/java/foodtruck/server/resources/json/TempTruckStopProvider.java
+++ main/src/main/java/foodtruck/server/resources/json/TempTruckStopProvider.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.WebApplicationException;
@@ -15,17 +17,22 @@ import javax.ws.rs.ext.Provider;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.inject.Inject;
 
+import foodtruck.geolocation.GeoLocator;
 import foodtruck.model.TempTruckStop;
 
 @Provider
 @Consumes(MediaType.APPLICATION_JSON)
 public class TempTruckStopProvider implements MessageBodyReader<TempTruckStop> {
 
+  private static final Logger log = Logger.getLogger(TempTruckStopProvider.class.getName());
+
   private final ObjectMapper mapper;
+  private final GeoLocator geoLocator;
 
   @Inject
-  public TempTruckStopProvider(ObjectMapper mapper) {
+  public TempTruckStopProvider(ObjectMapper mapper, GeoLocator geoLocator) {
     this.mapper = mapper;
+    this.geoLocator = geoLocator;
   }
 
   @Override
@@ -37,6 +44,10 @@ public class TempTruckStopProvider implements MessageBodyReader<TempTruckStop> {
   public TempTruckStop readFrom(Class<TempTruckStop> aClass, Type type, Annotation[] annotations, MediaType mediaType,
       MultivaluedMap<String, String> multivaluedMap,
       InputStream inputStream) throws IOException, WebApplicationException {
-    return mapper.readValue(inputStream, TempTruckStop.class);
+    TempTruckStop stop = mapper.readValue(inputStream, TempTruckStop.class);
+    // verify that the location exists
+    log.log(Level.INFO, "Updating temp stop {0}", stop.getLocationName());
+    geoLocator.locateOpt(stop.getLocationName()).orElseThrow( () -> new WebApplicationException(400));
+    return stop;
   }
 }
